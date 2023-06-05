1916b1b0a45895664940ebd751fa498cbb5d6b31
Ben Manicke <manicb@rpi.edu>
2023-06-05 19:32:24 -0400

    Homepage Frontend Changes
    Changes some of the alignments on the homepage and modified some more CSS to make it more accessible and welcoming. I also introduced some buttons that can link the user to our social media sites to learn more about us and show some support. These still need to be made functional but I still need to go through the process of creating the pages and what not.

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -8,25 +8,37 @@ function AboutUs() {
         <h1>About us</h1>
             <div className="headerSection">
                 <h2>Who we are:</h2>
+                <p>[Text will go here]</p>
+                <p>Hi Matt</p>
             </div>
-            {/* <div className="spinnerContainer">
-                <div className="spinner">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-            </div> */}
             <div className="headerSection">
                 <h2>Our Goals:</h2>
+                <p>[Text will go here]</p>
             </div>
-            <div className="headerSection">
+            <div className="socialHeaderSection">
                 <h2>How to Reach Us:</h2>
-            </div>
-            <div className="headerSection">
-                <h2>How to Support:</h2>
+                <ul class="wrapper">
+                    <li class="icon github">
+                        <span class="tooltip">GitHub</span>
+                        <span><i class="fab fa-github"></i></span>
+                    </li>
+                    <li class="icon discord">
+                        <span class="tooltip">Discord</span>
+                        <span><i class="fab fa-discord"></i></span>
+                    </li>
+                    <li class="icon twitter">
+                        <span class="tooltip">Twitter</span>
+                        <span><i class="fab fa-twitter"></i></span>
+                    </li>
+                    <li class="icon instagram">
+                        <span class="tooltip">Instagram</span>
+                        <span><i class="fab fa-instagram"></i></span>
+                    </li>
+                    <li class="icon facebook">
+                        <span class="tooltip">Facebook</span>
+                        <span><i class="fab fa-facebook-f"></i></span>
+                    </li>
+                </ul>
             </div>
         </div>
     </section>

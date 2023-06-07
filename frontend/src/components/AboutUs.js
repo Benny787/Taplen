114a922f3cc68671eaad281a6875bd2b906ed4e8
Ben Manicke <manicb@rpi.edu>
2023-06-07 10:37:47 -0400

    New Homepage Frontend Additions
    Not finished but in a much happier place with the "About Us" Section of the homepage. Have some text about myself and am now planning on working on the Calendar page for now.

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -5,39 +5,46 @@ function AboutUs() {
   return (
     <section className="main" id="aboutUsSection">
         <div className="headerContainer">
-        <h1>About us</h1>
+        <h1>About Taplen</h1>
             <div className="headerSection">
-                <h2>Who we are:</h2>
-                <p>[Text will go here]</p>
-                <p>Hi Matt</p>
+                <h2>Who developed Taplen</h2>
+                <p> Welcome! I am Ben Manicke, and I'm thrilled to have you here. As a developer, I am passionate about developing applications that showcase my abilities while helping and serving others.
+                    This website serves as a platform to test my abilities and provide users with an interactive and effective experience. I am committed to broadening my skills and abilities and sharing my experiences with you!
+                    I appreciate you checking out my site. Feel free to reach out and join me on this journey by checking out some of my other projects and checking out my socials!
+                </p>
             </div>
             <div className="headerSection">
-                <h2>Our Goals:</h2>
-                <p>[Text will go here]</p>
+                <h2>What Comes Next?</h2>
+                <p> My goal is to continue learning new skills and abilities and implementing them into my sites. I hope to document this journey by sharing these application with you! 
+                    I hope to meet some other developers and designers along the way and have them pass off any tips / critiques on my work. Again, I appreciate all of your support. You can 
+                    help out by by checking out some of my other projects, checking out my socials, or recommending the site to others. All feedback goes a really long way!
+
+
+                </p>
             </div>
             <div className="socialHeaderSection">
                 <h2>How to Reach Us:</h2>
-                <ul class="wrapper">
-                    <li class="icon github">
-                        <span class="tooltip">GitHub</span>
-                        <span><i class="fab fa-github"></i></span>
+                <ul className="wrapper">
+                    <li className="icon github">
+                        <span className="tooltip">GitHub</span>
+                        <span className="gitHubSpan"><a href="https://github.com/Benny787/Taplen" target="_blank"><i id="gitHubIcon" className="fab fa-github"></i></a></span>
                     </li>
-                    <li class="icon discord">
-                        <span class="tooltip">Discord</span>
-                        <span><i class="fab fa-discord"></i></span>
+                    <li className="icon discord">
+                        <span className="tooltip">Discord</span>
+                        <span><i className="fab fa-discord"></i></span>
                     </li>
-                    <li class="icon twitter">
-                        <span class="tooltip">Twitter</span>
-                        <span><i class="fab fa-twitter"></i></span>
+                    <li className="icon twitter">
+                        <span className="tooltip">Twitter</span>
+                        <span><i className="fab fa-twitter"></i></span>
                     </li>
-                    <li class="icon instagram">
-                        <span class="tooltip">Instagram</span>
-                        <span><i class="fab fa-instagram"></i></span>
+                    <li className="icon instagram">
+                        <span className="tooltip">Instagram</span>
+                        <span><i className="fab fa-instagram"></i></span>
                     </li>
-                    <li class="icon facebook">
-                        <span class="tooltip">Facebook</span>
-                        <span><i class="fab fa-facebook-f"></i></span>
-                    </li>
+                    {/* <li className="icon facebook">
+                        <span className="tooltip">Facebook</span>
+                        <span><i className="fab fa-facebook-f"></i></span>
+                    </li> */}
                 </ul>
             </div>
             <a id="heroSectionLink" href="#">Back to Top</a>
